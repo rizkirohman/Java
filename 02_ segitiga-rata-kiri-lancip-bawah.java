a2c56aa2551c7a0939243a06b264fc7883692704
Rizki Rohman <rizkirohman54@gmail.com>
2020-08-02 16:09:24 +0700

    Create 02_ segitiga-rata-kiri-lancip-bawah.java

---
+++ 02_ segitiga-rata-kiri-lancip-bawah.java
@@ -0,0 +1,11 @@
+class bintang {
+	public static void main(String[] args) {
+		int a, b;
+		for (a = 5; a >= 1; a--) {
+			for (b = 1; b <= a; b++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
